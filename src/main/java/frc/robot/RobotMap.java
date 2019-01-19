a2d6f1248b33f697f16e46d12a412eccc50e06f8
Sanjana679 <sanjana@nandi.net>
2019-01-18 17:24:20 -0800

    Wrote tank drive, fixed formatting on RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -23,38 +23,64 @@ public class RobotMap {
   // following variables to use with your drivetrain subsystem.
   // public static int leftMotor = 1;
   // public static int rightMotor = 2;
-
-  //TODO update all variables/input ports
-  static WPI_TalonSRX elevator = new WPI_TalonSRX(0);
-  static WPI_TalonSRX elevatorSlave = new WPI_TalonSRX(0);
-  static public double elevatorSpeed = 0.3;
-  public int elevatorEncoderLimit = 100;
-  static public DigitalInput elevatorBottomLimit = new DigitalInput(0);
-
-  public static int HatchMotor = 0;
-  public static double HatchMotorSpeed = 0;
-  public static int HatchLimitSwitchPort = 0; //TODO change this later
-  
-
+    
   // If you are using multiple modules, make sure to define both the port
   // number and the module. For example you with a rangefinder:
   // public static int rangefinderPort = 1;
   // public static int rangefinderModule = 1;
+  
+  /**Porst */
+  //drive ports
+  static int lFrontMotorPort = 0;   //TODO
+  static int lBackMotorPort = 0;    //TODO
+  static int rFrontMotorPort = 0;   //TODO
+  static int rBackMotorPort = 0;    //TODO
+  static int rEncoderPort1 = 0;     //TODO
+  static int rEncoderPort2 = 1;     //TODO
+  static int lEncoderPort1 = 0;     //TODO
+  static int lEncoderPort2 = 1;     //TODO
 
-  //TODO Change these ports once Electrical decides.
-  static int SolenoidPort1 = 1; 
-  static int SolenoidPort2 = 2; 
-  static int CompressorPort = 0;
-  static int HatchMotorPort = 3;
-  static int rEncoderPort1 = 0; 
-  static int rEncoderPort2 = 1; 
-  static int lEncoderPort1 = 0; 
-  static int lEncoderPort2 = 1;
+  //elevator ports
+  static int elevatorPort = 0;      //TODO
+  static int elevatorSlavePort = 0; //TODO
 
-  static DoubleSolenoid HatchPiston = new DoubleSolenoid(SolenoidPort1, SolenoidPort2);
+  //solenoid ports
+  static int SolenoidPort1 = 1;     //TODO
+  static int SolenoidPort2 = 2;     //TODO
+
+  //hatch ports 
+  static int HatchMotorPort = 3;    //TODO
+  public static int HatchLimitSwitchPort = 0; //TODO
+
+  /**Motors */
+  //Drive motors
+  static WPI_TalonSRX lFrontMotor = new WPI_TalonSRX(lFrontMotorPort);
+  static WPI_TalonSRX lBackMotor = new WPI_TalonSRX(lBackMotorPort);
+  static WPI_TalonSRX rFrontMotor = new WPI_TalonSRX(rFrontMotorPort);
+  static WPI_TalonSRX rBackMotor = new WPI_TalonSRX(rBackMotorPort);
+
+  //elevator motors
+  static WPI_TalonSRX elevatorMotor = new WPI_TalonSRX(elevatorPort);
+  static WPI_TalonSRX elevatorSlaveMotor = new WPI_TalonSRX(elevatorSlavePort);
+
+  //hatch motors/solenoids
   static WPI_TalonSRX HatchTalon = new WPI_TalonSRX(HatchMotorPort);
+  static DoubleSolenoid HatchPiston = new DoubleSolenoid(SolenoidPort1, SolenoidPort2);
+
+  /**Sensors */
+  //drive sensors
   public static Encoder rEncoder = new Encoder(rEncoderPort1, rEncoderPort2);
   public static Encoder lEncoder = new Encoder(lEncoderPort1, lEncoderPort2);
+  
+  //elevator sensors
+  static public DigitalInput elevatorBottomLimit = new DigitalInput(0);
+
+  //hatch sensors
   public static DigitalInput HatchLimitSwitch = new DigitalInput(HatchLimitSwitchPort);
 
+  //TODO update all variables/input ports
+  static public double elevatorSpeed = 0.3;
+  public int elevatorEncoderLimit = 100; //these are encoder ticks
+ 
+  public static double HatchMotorSpeed = 0;
 }
