1cede073bafa1235547f7dec64c4ac845d4e3ee8
Navneeth <navneeth.suresh@gmail.com>
2019-01-21 10:49:36 -0800

    Worked on MoveHatchDown command

--- src/main/java/frc/robot/commands/MoveHatchDown.java
+++ src/main/java/frc/robot/commands/MoveHatchDown.java
@@ -8,11 +8,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
 
 public class MoveHatchDown extends Command {
   public MoveHatchDown() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    requires(Robot.hatch);
   }
 
   // Called just before this Command runs the first time
@@ -23,22 +26,29 @@ public class MoveHatchDown extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.hatch.setMotorSpeed(RobotMap.hatchDownSpeed);
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    if(Robot.hatch.getBottomLimit()){
+      return true;
+    } else{
+      return false;
+    }
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.hatch.setMotorSpeed(0);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    end();
   }
 }
